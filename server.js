f670ba9628d18233009bc74326978404807f3ef7
Ryan Tseng <ryan.tseng@webcomm.com.tw>
2025-05-27 19:36:00 +0800

    update request size

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import dotenv from 'dotenv';
 const mecab = new MeCab();
 const app = express();
 dotenv.config();
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.post('/tokenizer', (req, res) => {
     console.log(req.body)
     mecab.parse(req.body.sentence, function(err, result) {
